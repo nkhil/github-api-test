8bad55eb9bf367095ed5b9a6410bb06cd4c7fda3
Nikhil Vijayan <nkhilv@gmail.com>
2018-11-16 21:41:53 +0000

    Added styling to the form and commented out all but the fetch API code

--- index.js
+++ index.js
@@ -54,88 +54,88 @@ function renderData(data) {
 
 // Handle form submit
 
-form2.addEventListener('submit', e => {
-    e.preventDefault();
-    console.log('XMLHttpRequest Form submitted + prevented..');
-    callApi2(e);
-});
+// form2.addEventListener('submit', e => {
+//     e.preventDefault();
+//     console.log('XMLHttpRequest Form submitted + prevented..');
+//     callApi2(e);
+// });
 
 // Get the API query URL dynamically and then send it
 
-function callApi2(e){
-    const searchQuery2 = document.getElementById('searchQuery2').value.toLowerCase() ;
-    const newEndpoint2 = 'https://api.github.com/users/' + searchQuery2;
-    console.log(newEndpoint2);
-    const request = new XMLHttpRequest();
+// function callApi2(e){
+//     const searchQuery2 = document.getElementById('searchQuery2').value.toLowerCase() ;
+//     const newEndpoint2 = 'https://api.github.com/users/' + searchQuery2;
+//     console.log(newEndpoint2);
+//     const request = new XMLHttpRequest();
 
-    request.open('GET', newEndpoint2);
-    request.onload = () => {
-        const requestData2 = JSON.parse(request.responseText);
+//     request.open('GET', newEndpoint2);
+//     request.onload = () => {
+//         const requestData2 = JSON.parse(request.responseText);
 
-        if(request.status >= 200 && request.status < 400) {
-            renderData2(requestData2);            
-        } else {
-            console.log('meh..');
-        }
-    }
-    request.onerror = () => console.log('error');
-    request.send();
-}
+//         if(request.status >= 200 && request.status < 400) {
+//             renderData2(requestData2);            
+//         } else {
+//             console.log('meh..');
+//         }
+//     }
+//     request.onerror = () => console.log('error');
+//     request.send();
+// }
 
 // Render the data out into the DOM
 
-function renderData2(data){
-    console.log(data);
-    console.log(data.bio);
-    const html = `
-        <h2>${data.login}</h2>
-        <h2>Full Name: ${data.name}</h2>
-        <img src="${data.avatar_url}" alt="">
-        <h3>${data.bio}</h3>
-        <p>
-        Personal website: <a href="http://${data.blog}" target="_blank">${data.blog}</a>
-        </p>
-    `;
-    container2.innerHTML = html;
-}
+// function renderData2(data){
+//     console.log(data);
+//     console.log(data.bio);
+//     const html = `
+//         <h2>${data.login}</h2>
+//         <h2>Full Name: ${data.name}</h2>
+//         <img src="${data.avatar_url}" alt="">
+//         <h3>${data.bio}</h3>
+//         <p>
+//         Personal website: <a href="http://${data.blog}" target="_blank">${data.blog}</a>
+//         </p>
+//     `;
+//     container2.innerHTML = html;
+// }
 
 
 /////////////////////////////////////////// Using Axios /////////////////////////////////////////
 
-form3.addEventListener('submit', e =>{
-    e.preventDefault();
-    console.log('Axios form submitted, default prevented');
-    callApi3(e);
-});
+// form3.addEventListener('submit', e =>{
+//     e.preventDefault();
+//     console.log('Axios form submitted, default prevented');
+//     callApi3(e);
+// });
 
-function callApi3(e){
-    const searchQuery3 = document.getElementById('searchQuery3').value.toLowerCase() ;
-    console.log(searchQuery3);
-    const newEndpoint3 = 'https://api.github.com/users/' + searchQuery3;
-    console.log(newEndpoint3);
+// function callApi3(e){
+//     const searchQuery3 = document.getElementById('searchQuery3').value.toLowerCase() ;
+//     console.log(searchQuery3);
+//     const newEndpoint3 = 'https://api.github.com/users/' + searchQuery3;
+//     console.log(newEndpoint3);
 
-    axios.get(newEndpoint3)
-        .then(res => {
-            console.log('axios result =>', res);
-            console.log(res.data);
-            renderData3(res.data);
-        })
-        .catch(err => {
-            console.log('ERROR! =>', err);
+//     axios.get(newEndpoint3)
+//         .then(res => {
+//             console.log('axios result =>', res);
+//             console.log(res.data);
+//             renderData3(res.data);
+//         })
+//         .catch(err => {
+//             console.log('ERROR! =>', err);
             
-        })
-}
+//         })
+// }
 
-function renderData3(data){
+// function renderData3(data){
     
-    const html = `
-        <h2>${data.login}</h2>
-        <h2>Full Name: ${data.name}</h2>
-        <img src="${data.avatar_url}" alt="">
-        <h3>${data.bio}</h3>
-        <p>
-        Personal website: <a href="http://${data.blog}" target="_blank">${data.blog}</a>
-        </p>
-    `;
-    container3.innerHTML = html;
-}
+//     const html = `
+//         <h2>${data.login}</h2>
+//         <h2>Full Name: ${data.name}</h2>
+//         <img src="${data.avatar_url}" alt="">
+//         <h3>${data.bio}</h3>
+//         <p>
+//         Personal website: <a href="http://${data.blog}" target="_blank">${data.blog}</a>
+//         </p>
+//     `;
+//     container3.innerHTML = html;
+// }
